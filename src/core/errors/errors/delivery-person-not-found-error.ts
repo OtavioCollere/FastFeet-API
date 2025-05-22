b511238887072f0df6e357648f8344944d6bbfca
otavioCollere <otaviosk59@gmail.com>
2025-05-22 12:19:21 -0300

    new config nestjs

--- src/core/errors/errors/delivery-person-not-found-error.ts
+++
@@ -1,7 +0,0 @@
-import type { UseCaseError } from '../use-case-error';
-
-export class DeliveryPersonNotFoundError extends Error implements UseCaseError {
-  constructor() {
-    super('Delivery Person not found.');
-  }
-}
--- src/core/errors/errors/recipient-not-found-error.ts
+++
@@ -1,7 +0,0 @@
-import type { UseCaseError } from '../use-case-error';
-
-export class RecipientNotFoundError extends Error implements UseCaseError {
-  constructor() {
-    super('Recipient not found.');
-  }
-}
